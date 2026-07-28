14758e7ff65ef437462091c13963fd52c9f81f51
Omitoi <pekubec@gmail.com>
2026-07-28 10:48:27 +0300

    initial

---
+++ myMonitors.lua
@@ -0,0 +1,14 @@
+hl.monitor({
+	output	=	"eDP-1",
+	mode	=	"1920x1080@144",
+	position	=	"0x216",
+	scale	=	"1.25",
+})
+
+-- Secondary BenQ home monitor
+hl.monitor({
+	output	=	"HDMI-A-1",
+	mode	=	"1920x1080@60",
+	position	=	"1536x0",
+	scale	=	"1",
+})
