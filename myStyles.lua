d7af9f96ebf57680a4feadb15d7a85a6e1ed0f3d
Omitoi <pekubec@gmail.com>
2026-07-28 12:08:13 +0300

    works

---
+++ myStyles.lua
@@ -0,0 +1,140 @@
+bg_dark        = "rgb(0b120c)"       -- Deep dark green/black base
+circuit_green  = "rgb(39ff14)"       -- Neon glowing circuit green
+circuit_dim    = "rgb(154a1d)"       -- Muted circuit green
+flame_core     = "rgb(ffee00)"       -- Bright core yellow
+flame_outer    = "rgb(ff8800)"       -- Burning orange
+
+hl.config({
+    general = {
+        gaps_in = 2,
+        gaps_out = 5,
+        border_size = 1,
+        col = {
+            -- Active border: 2-color gradient table + numerical angle
+            active_border = {
+                colors = { circuit_green, flame_core },
+                angle = 45,
+            },
+            -- Inactive border: Single color string
+            inactive_border = circuit_dim,
+        },
+
+        layout = "master",
+
+        resize_on_border    =  true,
+        no_focus_fallback   =  true,
+        allow_tearing   = false,
+    },
+
+    decoration = {
+        rounding = 6,
+        active_opacity = 1.0,
+        inactive_opacity = 0.85,
+        dim_inactive = true,
+        dim_strength = 0.25,
+
+        shadow = {
+            enabled = true,
+            range = 15,
+            render_power = 3,
+            color = "#0b120cdd",
+        },
+
+        blur = {
+            enabled = true,
+            size = 6,
+            passes = 2,
+            new_optimizations = true,
+        },
+    },
+
+    group = {
+        col = {
+            border_active = {
+                colors = { flame_core, flame_outer },
+                angle = 45,
+            },
+
+            border_inactive = circuit_dim,
+        },
+
+        groupbar = {
+            col = {
+                active = circuit_green,
+                inactive = circuit_dim,
+            },
+            text_color = bg_dark,
+        },
+    },
+
+    animations = {
+        enabled = true,
+    },
+})
+
+-- Default curves and animations, see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Animations/
+hl.curve("easeOutQuint",   { type = "bezier", points = { {0.23, 1},    {0.32, 1}    } })
+hl.curve("easeInOutCubic", { type = "bezier", points = { {0.65, 0.05}, {0.36, 1}    } })
+hl.curve("linear",         { type = "bezier", points = { {0, 0},       {1, 1}       } })
+hl.curve("almostLinear",   { type = "bezier", points = { {0.5, 0.5},   {0.75, 1}    } })
+hl.curve("quick",          { type = "bezier", points = { {0.15, 0},    {0.1, 1}     } })
+
+-- Default springs
+hl.curve("easy",           { type = "spring", mass = 1, stiffness = 71.2633, dampening = 15.8273644 })
+
+hl.animation({ leaf = "global",        enabled = true,  speed = 10,   bezier = "default" })
+hl.animation({ leaf = "border",        enabled = true,  speed = 5.39, bezier = "easeOutQuint" })
+hl.animation({ leaf = "windows",       enabled = true,  speed = 4.79, spring = "easy" })
+hl.animation({ leaf = "windowsIn",     enabled = true,  speed = 4.1,  spring = "easy",         style = "popin 87%" })
+hl.animation({ leaf = "windowsOut",    enabled = true,  speed = 1.49, bezier = "linear",       style = "popin 87%" })
+hl.animation({ leaf = "fadeIn",        enabled = true,  speed = 1.73, bezier = "almostLinear" })
+hl.animation({ leaf = "fadeOut",       enabled = true,  speed = 1.46, bezier = "almostLinear" })
+hl.animation({ leaf = "fade",          enabled = true,  speed = 3.03, bezier = "quick" })
+hl.animation({ leaf = "layers",        enabled = true,  speed = 3.81, bezier = "easeOutQuint" })
+hl.animation({ leaf = "layersIn",      enabled = true,  speed = 4,    bezier = "easeOutQuint", style = "fade" })
+hl.animation({ leaf = "layersOut",     enabled = true,  speed = 1.5,  bezier = "linear",       style = "fade" })
+hl.animation({ leaf = "fadeLayersIn",  enabled = true,  speed = 1.79, bezier = "almostLinear" })
+hl.animation({ leaf = "fadeLayersOut", enabled = true,  speed = 1.39, bezier = "almostLinear" })
+hl.animation({ leaf = "workspaces",    enabled = true,  speed = 1.94, bezier = "almostLinear", style = "fade" })
+hl.animation({ leaf = "workspacesIn",  enabled = true,  speed = 1.21, bezier = "almostLinear", style = "fade" })
+hl.animation({ leaf = "workspacesOut", enabled = true,  speed = 1.94, bezier = "almostLinear", style = "fade" })
+hl.animation({ leaf = "zoomFactor",    enabled = true,  speed = 7,    bezier = "quick" })
+
+-- Ref https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
+-- "Smart gaps" / "No gaps when only"
+-- uncomment all if you wish to use that.
+-- hl.workspace_rule({ workspace = "w[tv1]", gaps_out = 0, gaps_in = 0 })
+-- hl.workspace_rule({ workspace = "f[1]",   gaps_out = 0, gaps_in = 0 })
+-- hl.window_rule({
+--     name  = "no-gaps-wtv1",
+--     match = { float = false, workspace = "w[tv1]" },
+--     border_size = 0,
+--     rounding    = 0,
+-- })
+-- hl.window_rule({
+--     name  = "no-gaps-f1",
+--     match = { float = false, workspace = "f[1]" },
+--     border_size = 0,
+--     rounding    = 0,
+-- })
+
+-- See https://wiki.hypr.land/Configuring/Layouts/Dwindle-Layout/ for more
+hl.config({
+    dwindle = {
+        preserve_split = true, -- You probably want this
+    },
+})
+
+-- See https://wiki.hypr.land/Configuring/Layouts/Master-Layout/ for more
+hl.config({
+    master = {
+        new_status = "master",
+    },
+})
+
+-- See https://wiki.hypr.land/Configuring/Layouts/Scrolling-Layout/ for more
+hl.config({
+    scrolling = {
+        fullscreen_on_one_column = true,
+    },
+})
